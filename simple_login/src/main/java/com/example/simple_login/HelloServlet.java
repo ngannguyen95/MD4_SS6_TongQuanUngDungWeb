f1751853479f9c033d20767ab8c683195d19b50c
ngannguyen95 <119550498+ngannguyen95@users.noreply.github.com>
2023-04-26 10:52:29 +0700

    Add files via upload

---
+++ simple_login/src/main/java/com/example/simple_login/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.simple_login;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //trả về chuyển về chữ tiếng việt
+        response.setCharacterEncoding("UTF-8");
+        // trả về dưới dạng text và html
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
